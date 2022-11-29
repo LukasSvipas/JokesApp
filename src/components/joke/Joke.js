b16d368b1553d39f314d48221a7fc664a2bfe246
LukasSvipas <113436969+LukasSvipas@users.noreply.github.com>
2022-11-29 20:19:57 +0200

    commit

--- src/components/joke/Joke.js
+++ src/components/joke/Joke.js
@@ -1,8 +1,10 @@
 import {Card, Button} from "react-bootstrap";
 import './joke.scss';
+import { useState } from "react";
 let favoriteList =[];
 const Joke=(props)=>{
-
+    const [isVisible, setIsVisible] = useState(true);
+    const toggleVisibility = () => { setIsVisible(!isVisible)};
     const handleSave = () => {
         let obj ={}
         obj["id"] = props.id;
@@ -16,7 +18,7 @@ const Joke=(props)=>{
             <Card.Header className="bg-success p-2 text-dark bg-opacity-25">{props.value}</Card.Header>
             <Card.Body>
                 <div className="my-2"><a href={props.url} target="blank">Link</a></div>
-                <div><Button className="btn btn-dark btn-sm" onClick={handleSave}>Add to My Jokes</Button></div>
+                {isVisible && <div><Button className="btn btn-dark btn-sm" onClick={() => { handleSave(); toggleVisibility(); }}>Add to My Jokes</Button></div>}
             </Card.Body>
         </Card>
     )
